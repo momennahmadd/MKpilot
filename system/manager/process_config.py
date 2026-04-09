ac12c2d5d8b695346a0c993c6d0f2815347330f5
Momen Khedr <momennahmadd@gmail.com>
2026-04-09 15:38:27 +0200

    Cabana launch script enhancing to choose venv && changing process_config.py to record and log without ignition

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -64,13 +64,13 @@ def and_(*fns):
 procs = [
   DaemonProcess("manage_athenad", "system.athena.manage_athenad", "AthenadPid"),
 
-  NativeProcess("loggerd", "system/loggerd", ["./loggerd"], logging),
-  NativeProcess("encoderd", "system/loggerd", ["./encoderd"], only_onroad),
+  NativeProcess("loggerd", "system/loggerd", ["./loggerd"], always_run),
+  NativeProcess("encoderd", "system/loggerd", ["./encoderd"], always_run),
   NativeProcess("stream_encoderd", "system/loggerd", ["./encoderd", "--stream"], notcar),
   PythonProcess("logmessaged", "system.logmessaged", always_run),
 
-  NativeProcess("camerad", "system/camerad", ["./camerad"], driverview, enabled=not WEBCAM),
-  PythonProcess("webcamerad", "tools.webcam.camerad", driverview, enabled=WEBCAM),
+  NativeProcess("camerad", "system/camerad", ["./camerad"], always_run, enabled=not WEBCAM),
+  PythonProcess("webcamerad", "tools.webcam.camerad", always_run, enabled=WEBCAM),
   PythonProcess("proclogd", "system.proclogd", only_onroad, enabled=platform.system() != "Darwin"),
   PythonProcess("journald", "system.journald", only_onroad, platform.system() != "Darwin"),
   PythonProcess("micd", "system.micd", iscar),
